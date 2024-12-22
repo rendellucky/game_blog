412bf67ba4dc39eb613af047d972fa9c125b930a
rendellucky <antonkonev2003@gmail.com>
2024-12-22 18:26:06 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,19 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
   plugins: [
     laravel({
-      input: [
-        'resources/css/app.css',
-        'resources/js/app.js',
-      ],
+      input: ['resources/css/app.css', 'resources/js/app.js'],
       refresh: true,
     }),
   ],
   build: {
     manifest: true,
     outDir: 'public/build',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      },
+    },
   },
 });
